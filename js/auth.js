2482168f0d45afbc7a2c1b4b86a19a26a876117e
Puneet Mishra <punitm.916@gmail.com>
2020-11-24 00:55:45 +0530

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,18 +1,1 @@
-
-const loginForm = document.getElementById("login-form");
-const loginButton = document.getElementById("login-form-submit");
-const loginErrorMsg = document.getElementById("login-error-msg");
-
-loginButton.addEventListener("click", (e) => {
-e.preventDefault();
-const username = loginForm.username.value;
-const password = loginForm.password.value;
-
-if (username === "riddhu" && password === "dotb") {
-    //alert("You have successfully logged in.");
-    location.replace("riddhu_eDDimklmkJNIgvbuycrtf.html");
-} else {
-    //loginErrorMsg.style.opacity = 1;
-    alert("Incorrect Username/Password");
-}
-})
+ const loginForm = document.getElementById("login-form"); const loginButton = document.getElementById("login-form-submit"); const loginErrorMsg = document.getElementById("login-error-msg"); loginButton.addEventListener("click", (e) => {e.preventDefault(); const username = loginForm.username.value; const password = loginForm.password.value; if (username === "riddhu" && password === "dotb") {//alert("You have successfully logged in."); location.replace("riddhu_eDDimklmkJNIgvbuycrtf.html"); } else {//loginErrorMsg.style.opacity = 1; alert("Incorrect Username/Password"); } })
