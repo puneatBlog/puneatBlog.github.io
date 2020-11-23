6d07e4915639ed15fbe7bfe584ad2c7910c50195
Puneet Mishra <punitm.916@gmail.com>
2020-11-23 20:50:57 +0530

    added login page

---
+++ js/auth.js
@@ -0,0 +1,18 @@
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "riddhu" && password === "dotp") {
+        //alert("You have successfully logged in.");
+        location.replace("riddhu_eDDimklmkJNIgvbuycrtf.html");
+    } else {
+        //loginErrorMsg.style.opacity = 1;
+        alert("Incorrect Username/Password");
+    }
+})
