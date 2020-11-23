4105a85005a298cde112cd94d925d67b91fcf7cd
Puneet Mishra <punitm.916@gmail.com>
2020-11-24 00:50:13 +0530

    completed spotify embedding

--- js/auth.js
+++ js/auth.js
@@ -4,15 +4,15 @@ const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
+e.preventDefault();
+const username = loginForm.username.value;
+const password = loginForm.password.value;
 
-    if (username === "riddhu" && password === "dotp") {
-        //alert("You have successfully logged in.");
-        location.replace("riddhu_eDDimklmkJNIgvbuycrtf.html");
-    } else {
-        //loginErrorMsg.style.opacity = 1;
-        alert("Incorrect Username/Password");
-    }
+if (username === "riddhu" && password === "dotb") {
+    //alert("You have successfully logged in.");
+    location.replace("riddhu_eDDimklmkJNIgvbuycrtf.html");
+} else {
+    //loginErrorMsg.style.opacity = 1;
+    alert("Incorrect Username/Password");
+}
 })
